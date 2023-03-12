f25d1deeac70f3ea0d1b2d23f5d642de7d448aab
David Li <davidli012345@gmail.com>
2023-03-11 17:04:49 -0800

    Fix: again

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from openbb_terminal.forecast.whisper_model import transcribe_and_summarize
 
 def get_video_id(url):
 
-    video_id = re.findall(r"v=([-\w]{11})", url)[0]
+    video_id = re.findall(r"v=([\w]{11})", url)[0]
     old_stdin = sys.stdin
     # mkdir /home/user/.cache/whisper
     os.makedirs(f"/home/user/.cache/whisper", exist_ok=True)
@@ -21,9 +21,17 @@ def get_video_id(url):
         return "Please enter a YouTube URL"
     sys.stdin = old_stdin
     print(f"Video ID: {video_id}")
-    summary_file = glob.glob(f"-{video_id}/*_summary.txt")[0]
+    try:
+        summary_file = glob.glob(f"{video_id}/*_summary.txt")[0]
+    except Exception as e:
+        # get latest file with *_summary.txt
+        summary_file = max(glob.glob(f"**/*_summary.txt"), key=os.path.getctime)
     # file .srt file
-    subtitle_file = glob.glob(f"-{video_id}/*.srt")[0] or glob.glob(f"{video_id}/*.vtt")[0]
+    try:
+        subtitle_file = glob.glob(f"{video_id}/*.srt")[0] or glob.glob(f"{video_id}/*.vtt")[0]
+    except Exception as e:
+        # get latest file with .srt or .vtt
+        subtitle_file = max(glob.glob(f"**/*.srt"), key=os.path.getctime) or max(glob.glob(f"**/*.vtt"), key=os.path.getctime)
     return summary_file, subtitle_file
 
 input_text = gr.inputs.Textbox(label="Enter a YouTube URL")
